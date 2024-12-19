4385f2f0373295832fca6e2c70c55d9e39b12912
ztcoalson <ztcoalson@gmail.com>
2024-12-19 12:35:55 -0800

    Update

--- run_search.sh
+++ run_search.sh
@@ -1,7 +1,15 @@
+#!/bin/bash
+#SBATCH -J nsga
+#SBATCH -A eecs
+#SBATCH -p dgx2
+#SBATCH --mem=50G
+#SBATCH --gres=gpu:1
+#SBATCH -t 7-00:00:00
+
 python msunas.py \
     --dataset cifar10 --data ../data \
     --save search-TEST \
-    --sec_obj None \
+    --sec_obj flops \
     --n_gpus 1 \
     --supernet_path ./supernets/ofa_mbv3_d234_e346_k357_w1.0 \
     --vld_size 5000 \
